85a21430909ecdea045d3dde602c85259d2d179a
florentine-wiegand <florentine.wiegand@gmail.com>
2026-03-20 10:47:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getSortedSummariesData } from '@/lib/api';
 import Link from 'next/link';
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   const allSummariesData = getSortedSummariesData();
